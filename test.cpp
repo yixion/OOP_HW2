63ecc1961648e78c2ba8b122c613b26fd03fc7f2
YexionLin <lhl4040294@gmail.com>
2024-06-22 13:02:45 +0800

    HW3

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string test = "12345678";
+    cout<<test[0]<<endl;
+    if(test[0] == '1'){
+        cout<<"hi"<<endl;
+    }
+    return 0;
+}
